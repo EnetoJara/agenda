7b9055edbff91d2956279373fbfd499313c0c10d
Ernesto Jara Olveda <ernesto.jara06@gmail.com>
2020-05-06 19:08:58 -0500

    Selectors

--- client/src/modules/lists/lists-selectors.ts
+++ client/src/modules/lists/lists-selectors.ts
@@ -1,11 +1,11 @@
-import { createSelector } from "reselect";
-import { AppState, ListState, ListsState } from "@eneto/api-client";
+import { AppState, ListState } from "@eneto/api-client";
 import { Selector } from "react-redux";
+import { createSelector, OutputSelector } from "reselect";
 
-const getLists = (state: AppState) => state.lists.lists;
+const getLists: Selector<AppState, ListState[]> = (state: AppState): ListState[] => state.lists.lists;
 const getList: Selector<AppState, ListState> = (state: AppState): ListState => state.currentList;
-export const lists = createSelector(
+export const lists: OutputSelector<AppState, ListState[], (res: ListState[]) => ListState[]> = createSelector<AppState, ListState[], ListState[]>(
     getLists,
     lists => lists,
 );
-export const list = createSelector<AppState, ListState, ListState>(getList, (lists: ListState): ListState => lists);
+export const list: OutputSelector<AppState, ListState, (res: ListState) => ListState> = createSelector<AppState, ListState, ListState>(getList, (lists: ListState): ListState => lists);
